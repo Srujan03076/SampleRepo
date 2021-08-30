c5a5ec7757145540670e2e48682c5ddba5c56445
unknown <srujanmahaja5@gmail.com>
2021-08-30 12:13:41 +0530

    Adding NewProject

---
+++ Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp059
+{
+    class Car
+    {
+
+ public void Drive()
+        {
+            Console.WriteLine("Driving");
+        }
+        public static void Brake()
+        {
+            Console.WriteLine("Brake");
+        }
+        static void Main(string[]args)
+        {
+            Car myobj = new Car();
+            myobj.Drive();
+            Brake();
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+
+  
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp059
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myobj = new Car();
+            myobj.Drive();
+            Car.Brake();
+        }
+    }
+}
+    
+
+
+           
+        
+      
+         
+      
+
+
+
+
